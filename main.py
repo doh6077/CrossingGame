9c9cd9eefa82e727eef6a970af2c5893de2f7d59
82108 <doh6077@gmail.com>
2023-10-08 21:19:17 -0400

    Added pygame library

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import time
 from turtle import Screen
+import pygame
 
 from car import Car
 from player import Player, FINISH_LINE_Y
@@ -8,13 +9,11 @@ from scoreboard import Scoreboard
 IMAGE ="car.gif"
 PLAYER1_STARTING_POSITION = (-50, -280)
 PLAYER2_STARTING_POSITION = (50, -280)
+screen_width = 480
+screen_height = 640
 
-screen = Screen()
-screen.setup(width=400, height=600)
-screen.tracer(0)
-screen.bgpic("cross2.png")
-screen.update()
-screen.addshape(IMAGE)
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption("Crossing Game")
 car = Car()
 scoreboard = Scoreboard()
 
@@ -32,23 +31,28 @@ screen.onkey(player2.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_is_on = False
 
-    time.sleep(0.1)
-    screen.update()
+        time.sleep(0.1)
+        screen.update()
 
-    car.create_car()
-    car.move_cars()
+        car.create_car()
+        car.move_cars()
 
-    for i in range(len(car.all_cars)):
-        if car.all_cars[i].distance(player1) < 20:
+        for i in range(len(car.all_cars)):
+            if car.all_cars[i].distance(player1) < 20:
+                player1.reset_position()
+
+            if car.all_cars[i].distance(player2) < 20:
+                player2.reset_position()
+
+        if player1.ycor() > FINISH_LINE_Y and player2.ycor() > FINISH_LINE_Y:
             player1.reset_position()
-
-        if car.all_cars[i].distance(player2) < 20:
             player2.reset_position()
+            car.increaseSpeed()
+            scoreboard.addscore()
+            scoreboard.increase_level()
 
-    if player1.ycor() > FINISH_LINE_Y and player2.ycor() > FINISH_LINE_Y:
-        player1.reset_position()
-        player2.reset_position()
-        car.increaseSpeed()
-        scoreboard.addscore()
-        scoreboard.increase_level()
+pygame.quit()
